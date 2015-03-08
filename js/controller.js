bdae588586ca11cb606c0d4b0cba99ba5176a8e7
Aditya Shankar <adityashankar@Adityas-MacBook-Pro.local>
2015-03-08 15:38:08 -0400

    added control with gestures

--- js/controller.js
+++ js/controller.js
@@ -10,3 +10,60 @@ document.body.onkeydown = function( e ) {
         render();
     }
 };
+
+
+function exec_swipe(gesture)
+{
+    var isHorizontal = Math.abs(gesture.direction[0]) > Math.abs(gesture.direction[1]);
+    //Classify as right-left or up-down
+    if(isHorizontal)
+    {
+      if(gesture.direction[0] > 0)
+      {
+          swipeDirection = "right";
+          keyPress('right');
+      } 
+      else 
+      {
+          swipeDirection = "left";
+          keyPress('left');
+      }
+   }
+    else 
+    { //vertical
+        if(gesture.direction[1] > 0)
+        {
+          swipeDirection = "up";
+        } else 
+        {
+          swipeDirection = "down";
+          keyPress('down');
+        }                  
+    }
+    console.log(swipeDirection);
+};
+
+var controller = Leap.loop({enableGestures: true}, function(frame){
+  console.log("got a frame");
+});
+
+controller.on("gesture", function(gesture){
+  //... handle gesture object
+  console.log("got a gesture");
+  switch (gesture.type){
+          case "circle":
+              console.log("Circle Gesture");
+              keyPress("rotate");
+              break;
+          case "keyTap":
+              console.log("Key Tap Gesture");
+              break;
+          case "screenTap":
+              console.log("Screen Tap Gesture");
+              break;
+          case "swipe":
+              console.log("Swipe Gesture");
+              exec_swipe(gesture);
+              break;
+        }
+});
